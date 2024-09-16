8e483190f2eb717490719f2d5ce8ef63ff2a6608
Edilberto Mario Rodríguez Fontalvo <edimarod@outlook.es>
2024-09-16 03:04:58 +0000

    fix: base url fixed

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Edimar-star/WEB-FE-REACT-HW4"
+  base: "https://github.com/Edimar-star/WEB-FE-REACT-HW4/"
 })
